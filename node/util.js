8a2eb4f2985f00f0e547c3512e46e66ff629c72a
hedonicadapter <filtersome@gmail.com>
2024-01-27 10:10:43 +0100

    move to submodule

--- node/util.js
+++
@@ -1,8 +0,0 @@
-class HttpError extends Error {
-  constructor(message, statusCode) {
-    super(message); // Pass the message to the Error constructor
-    this.statusCode = statusCode;
-  }
-}
-
-module.exports = HttpError;
